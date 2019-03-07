0694a154e720f1f8528c8250a1363b7ee4b038be
aleksioconnor <aleksi.oconnor@gmail.com>
2019-03-07 16:30:47 +0200

    finl changes

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,56 +1,119 @@
 import React, { useState } from 'react'
 
+const Persons = ({persons, filterValue}) => {
+  const filteredNames = persons.filter(person => person.name.toUpperCase().includes(filterValue.toUpperCase()));
+  const names = filteredNames.map(person =>
+    <li key={person.name}>
+        {person.name} {person.number}
+    </li>
+  )
+
+  return (
+      <div>
+        <ul>
+          {names}
+        </ul>
+      </div>
+  )
+}
+
+const Filter = (props) => {
+  return (
+    <div>
+      <input 
+        value={props.filterValue}
+        onChange={props.handleFilterChange}
+      />
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <div>
+      <form onSubmit={props.addName}>
+        <div>
+          nimi: 
+          <input 
+            value={props.newName}
+            onChange={props.handleNameChange} 
+          />
+        </div>
+
+        <div>
+          numero:
+            <input 
+              value={props.newNumber}
+              onChange={props.handleNumberChange}
+            />
+        </div>
+
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+    </div>
+  )
+}
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' },
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filterValue, setFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault();
-    const newObj = { name: newName};
-    setPersons(persons.concat(newObj));
+    if(persons.some(item => item.name === newName)) {
+        alert(`${newName} on jo luettelossa`);
+    }
+    else {
+      const newObj = { 
+        name: newName,
+        number: newNumber,
+      };
+      setPersons(persons.concat(newObj));
+    }
   }
 
   const handleNameChange = (event) => {
-      setNewName(event.target.value) // event is input field
+      setNewName(event.target.value)
   }
 
-  const Names = () => {
-      const names = persons.map(list =>
-          <li key={list.name}>
-              {list.name}
-          </li>
-      )
-      return (
-          <div>
-            <ul>
-              {names}
-            </ul>
-          </div>
-      )
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
-      <form onSubmit={addName}>
-        <div>
-          nimi: 
-          <input 
-            value={newName}
-            onChange={handleNameChange} 
-          />
-        </div>
-        <div>
-          <button type="submit">lisää</button>
-        </div>
-      </form>
-      <h2>Numerot</h2>
-      <Names />
+      <Filter 
+        handleFilterChange={handleFilterChange}
+        filterValue={filterValue}
+      />
+      <h3>Lisää uusi</h3>
+      <PersonForm 
+        addName={addName}
+        handleNameChange={handleNameChange}
+        newName={newName}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange} 
+      />
+      <h3>Numerot</h3>
+      <Persons 
+        persons={persons}
+        filterValue={filterValue}
+      />
     </div>
   )
-
 }
 
 export default App
